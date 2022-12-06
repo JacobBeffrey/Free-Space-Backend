f07e3815da3cf55931be41141bc38dabb1c1a7fd
Amy Chico <boomaster40@gmail.com>
2022-12-06 13:13:17 -0500

    comment deletion

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,11 +9,3 @@ app.use(express.json());
 app.use("/pictureOfTheDay", PictureOfTheDayRouter);
 app.use("/calendar", calendarEventRouter);
 export const api = functions.https.onRequest(app);
-
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
