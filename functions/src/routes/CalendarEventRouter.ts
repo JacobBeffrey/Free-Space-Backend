8faa6402579256ed98130b0744f72f6d3f61df88
Jacob Beffrey <jacob.beffrey@Gmail.com>
2022-12-15 16:25:16 -0500

    Last commit before graduation

--- functions/src/routes/CalendarEventRouter.ts
+++ functions/src/routes/CalendarEventRouter.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { getClient } from "../db";
 import CalendarEvent from "../models/CalendarEvent";
+import axios from "axios";
 
 const calendarEventRouter = express.Router();
 const errorResponse = (error: any, res: any) => {
@@ -12,6 +13,16 @@ calendarEventRouter.get("/all-events", async (req, res) => {
     const client = await getClient();
     const cursor = client.db().collection<CalendarEvent>("events").find();
     const results = await cursor.toArray();
+    res.json(results);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+calendarEventRouter.get("/iss", async (req, res) => {
+  try {
+    const results = (await axios.get("http://api.open-notify.org/astros.json"))
+      .data;
+    res.status(200);
     res.json(results);
   } catch (err) {
     errorResponse(err, res);
